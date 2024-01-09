881988f78f6413053f0c91fb7c4ad34c7dd2811f
Nick Hodges <nickhodges@Nicks-MBP.home>
2024-01-09 18:18:47 -0500

    initial setup with navbar

---
+++ celesteApp/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { HeroComponent } from "./components/hero/hero.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, NavbarComponent, HeroComponent]
+})
+export class AppComponent {
+  title = 'celesteApp';
+}
