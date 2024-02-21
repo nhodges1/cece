d6991c953ff6e920e9513268b1f20b32dafd9ba5
Nick Hodges <nickhodges@Nicks-MBP.home>
2024-02-20 21:18:16 -0500

    Completed homepage

--- celesteApp/src/app/app.component.ts
+++ celesteApp/src/app/app.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, Inject, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { HeroComponent } from "./components/hero/hero.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import Aos from "aos";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, NavbarComponent, HeroComponent]
+    imports: [CommonModule, RouterOutlet, NavbarComponent, HeroComponent, FooterComponent]
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'celesteApp';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  
+  ngAfterViewInit() {
+    // Check if the code is running in a browser environment
+    if (isPlatformBrowser(this.platformId)) {
+      // Initialize AOS here after the view has been initialized
+      Aos.init();
+    }
+  }
 }
